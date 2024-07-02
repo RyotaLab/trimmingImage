0e1c6849a6f7941dfcb9cd5bc5c68ce8f842672b
RyotaLab <orengi12260@gmail.com>
2024-07-02 15:32:50 +0900

    Initial Commit

---
+++ makePhotoApp/makePhotoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  makePhotoAppApp.swift
+//  makePhotoApp
+//
+//  Created by 渡邊涼太 on 2024/07/02.
+//
+
+import SwiftUI
+
+@main
+struct makePhotoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
